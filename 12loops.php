43b932a1c2503789d247224ce5924a6b236bd0aa
Janki-001 <84764753+Janki-001@users.noreply.github.com>
2021-06-11 10:36:24 +0530

    Add files via upload

---
+++ 12loops.php
@@ -0,0 +1,26 @@
+<?php
+
+echo"while loops in php <br>";
+//echo 1; echo "<br>"; 
+//echo 2; echo "<br>"; 
+//echo 3; echo "<br>"; 
+//echo 4; echo "<br>"; 
+//echo 5; echo "<br>"; 
+$i=0;
+
+while ($i<5){
+    echo "the value if i is ";
+    echo $i+1;
+    echo"<br>";
+    $i+=3;
+    
+
+}
+
+
+
+
+
+
+
+?>
